5dc6160f0978b0eec6ef5c0e0da1ace6151215f6
safiyafikry <safiyafikry@gmail.com>
2023-02-02 13:04:48 +0200

    icons & imgs & strings &  colors are added

---
+++ app/src/main/java/eg/gov/iti/jets/mad/foodplanner/signupScreen/SignupScreen.java
@@ -0,0 +1,16 @@
+package eg.gov.iti.jets.mad.foodplanner.signupScreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import eg.gov.iti.jets.mad.foodplanner.R;
+
+public class SignupScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_screen);
+    }
+}
