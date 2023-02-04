ac3aef5d5537d489530a7bd7916cab0c4a7a8a18
safiyafikry <safiyafikry@gmail.com>
2023-02-04 14:37:25 +0200

    favorite screen and carditem are added

---
+++ app/src/main/java/eg/gov/iti/jets/mad/foodplanner/FavScreenFragment.java
@@ -0,0 +1,26 @@
+package eg.gov.iti.jets.mad.foodplanner;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FavScreenFragment extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_fav_screen, container, false);
+    }
+}
