562ce8f04248e83d2e901599740e556d6556e4bd
safiyafikry <safiyafikry@gmail.com>
2023-02-07 02:26:32 +0200

    google button is added

--- app/src/main/java/eg/gov/iti/jets/mad/foodplanner/Profile/ProfileFragment.java
+++ app/src/main/java/eg/gov/iti/jets/mad/foodplanner/Profile/ProfileFragment.java
@@ -11,6 +11,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 import eg.gov.iti.jets.mad.foodplanner.R;
 import eg.gov.iti.jets.mad.foodplanner.welcomeScreen.WelcomeScreen;
@@ -19,6 +28,11 @@ import eg.gov.iti.jets.mad.foodplanner.welcomeScreen.WelcomeScreen;
 public class ProfileFragment extends Fragment {
 
     Button logoutBtn;
+    FirebaseAuth auth;
+    GoogleSignInOptions gso;
+    GoogleSignInClient gsc;
+    TextView name_textView;
+    TextView email_TextView;
     public ProfileFragment() {
         // Required empty public constructor
     }
@@ -41,11 +55,30 @@ public class ProfileFragment extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         logoutBtn=view.findViewById(R.id.logoutButton);
+        name_textView=view.findViewById(R.id.userName);
+        email_TextView=view.findViewById(R.id.email);
+        auth=FirebaseAuth.getInstance();
+
+        gso =new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestEmail().build();
+        gsc = GoogleSignIn.getClient(getContext(),gso);
+
+        GoogleSignInAccount account=GoogleSignIn.getLastSignedInAccount(getContext());
+        if(account!=null){
+            name_textView.setText(account.getDisplayName());
+            email_TextView.setText(account.getEmail());
+        }
         logoutBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i =new Intent(getContext(), WelcomeScreen.class);
-                startActivity(i);
+               gsc.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                   @Override
+                   public void onComplete(@NonNull Task<Void> task) {
+                       Intent i =new Intent(getContext(), WelcomeScreen.class);
+                       i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+                       startActivity(i);
+                   }
+               });
+
             }
         });
     }
--- app/src/main/java/eg/gov/iti/jets/mad/foodplanner/welcomeScreen/WelcomeScreen.java
+++ app/src/main/java/eg/gov/iti/jets/mad/foodplanner/welcomeScreen/WelcomeScreen.java
@@ -1,15 +1,35 @@
 package eg.gov.iti.jets.mad.foodplanner.welcomeScreen;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
 
 import eg.gov.iti.jets.mad.foodplanner.Home.HomeFragment;
 import eg.gov.iti.jets.mad.foodplanner.MainActivity;
+import eg.gov.iti.jets.mad.foodplanner.Model.User;
 import eg.gov.iti.jets.mad.foodplanner.R;
 import eg.gov.iti.jets.mad.foodplanner.loginScreen.LoginScreenActivity;
 import eg.gov.iti.jets.mad.foodplanner.signupScreen.SignupScreen;
@@ -19,6 +39,11 @@ TextView alreadyHaveAnAccountTextView;
     TextView loginTextView;
     Button skip ;
     Button signUpBtn;
+    ImageButton googleBtn;
+    private GoogleSignInClient client;
+    FirebaseAuth auth;
+    FirebaseDatabase database;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +52,23 @@ TextView alreadyHaveAnAccountTextView;
         loginTextView=findViewById(R.id.login_TextView);
         signUpBtn=findViewById(R.id.signupWithEmail_btn);
         skip=findViewById(R.id.skip_btn);
+        googleBtn = findViewById(R.id.google_btn);
+        database=FirebaseDatabase.getInstance("https://foodplanner-78891-default-rtdb.firebaseio.com/");
+
+        auth=FirebaseAuth.getInstance();
+        GoogleSignInOptions options=new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        client= GoogleSignIn.getClient(this,options);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = client.getSignInIntent();
+                startActivityForResult(i,123);
+            }
+        });
         alreadyHaveAnAccountTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -58,5 +100,38 @@ TextView alreadyHaveAnAccountTextView;
                 startActivity(i);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==123){
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try{
+                GoogleSignInAccount account =task.getResult(ApiException.class);
+                AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(),null);
+                auth.signInWithCredential(credential).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        if(task.isSuccessful()){
+                            FirebaseUser user=auth.getCurrentUser();
+                            User user1=new User();
+                            assert user1 != null;
+                            user1.setEmail(user.getEmail());
+                            user1.setName(user.getDisplayName());
+                            database.getReference().child("User").child(user.getDisplayName()).setValue(user1);
+                            Intent i =new Intent(WelcomeScreen.this,MainActivity.class);
+                            startActivity(i);
+                        }
+                        else {
+                            Toast.makeText(WelcomeScreen.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+            catch (ApiException e){
+                e.printStackTrace();
+            }
+        }
     }
 }
